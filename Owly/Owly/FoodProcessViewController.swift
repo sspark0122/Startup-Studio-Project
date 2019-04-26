20492b0ecd1946ec94a373e5847f72780ccf8eda
Carolyn <haitc367@newschool.edu>
2019-04-26 17:47:31 -0400

    progress food

---
+++ Owly/Owly/FoodProcessViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FoodProcessViewController.swift
+//  Owly
+//
+//  Created by Carolyn Haitz Olaguivel on 4/26/19.
+//  Copyright © 2019 carolaguivel. All rights reserved.
+//
+
+import UIKit
+
+class FoodProcessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    
+    
+
+}
