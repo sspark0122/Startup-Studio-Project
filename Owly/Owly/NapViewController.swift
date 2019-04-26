53b799b947b25499eec59fe53d3fc1d7866929d7
Carolyn <haitc367@newschool.edu>
2019-04-26 18:17:13 -0400

    nap report

---
+++ Owly/Owly/NapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NapViewController.swift
+//  Owly
+//
+//  Created by Carolyn Haitz Olaguivel on 4/26/19.
+//  Copyright © 2019 carolaguivel. All rights reserved.
+//
+
+import UIKit
+
+class NapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
